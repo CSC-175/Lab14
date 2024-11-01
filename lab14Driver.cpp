ee244ba8b7e7b2004b5be7a1a16045f21fe35ba3
brmcbrid <69523108+brmcbrid@users.noreply.github.com>
2024-11-01 12:56:29 -0400

    Initial Commit

--- lab14Driver.cpp
+++ lab14Driver.cpp
@@ -7,18 +7,17 @@ int main()
 {
     char again; //variable that holds users decision to try again
     string denomination; //variable that will hold face value names
-    int chg; //variable that holds the current amount to be counted back
     int num; //variable that holds the number of each denomination
-    double dollarAmount; //dollar amount entered by the user to count back
+    double dollarAmount; //dollar amount entered by the user to count back as change
 
     do //start of loop to covert another amount
     {
-        cout << "\nEnter an amount to count back: ";
+        cout << "\nEnter an amount to count back $";
         cin >> dollarAmount;
         cout << endl;
-        while (chg > 0) //oop that repeats until chg equals zero
+        while (dollarAmount > 0) //oop that repeats until chg equals zero
         {
-            countBack(denomination, num, chg); //function call to convert chg to coins
+            countBack(denomination, num, dollarAmount); //function call to convert dollarAmount to coins
             cout << num << denomination << endl; //output of the number of each denomination
         }
         cout<<"\n\nWant to try another amount? (y/n): "; //prompt for user to repeat conversion
