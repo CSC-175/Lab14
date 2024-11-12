6bc127c8b4801f323ffb857f75f3453f1acf432a
brmcbrid <69523108+brmcbrid@users.noreply.github.com>
2024-11-11 22:04:38 -0500

    Initial Commit

--- Tests/test2.cpp
+++ Tests/test2.cpp
@@ -1,23 +1,8 @@
 #include <iostream>
-#include "../lab14.cpp"
+#include "../lab16.cpp"
 using namespace std;
 
-int main()
-{
-    char again; //variable that holds users decision to try again
-    string denomination; //variable that will hold face value names
-    double chg; //variable that holds the current amount to be counted back
-    int num; //variable that holds the number of each denomination
-    cin >> chg;
-    // chg = 12.87;
-
-	while (chg > 0) //loop that repeats until chg equals zero
-	{
-		countBack(denomination, num, chg); //function call to convert chg to coins
-		cout << num << denomination << ", "; //output of the number of each denomination
-	}
-
-
-    return 0;
+int main() {
+    cout << gcf(108,456) << endl;
+    // gcf 12
 }
-
